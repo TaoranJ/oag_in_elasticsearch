2059966ea15d9ef8d7d28e409c27135e10ff4860
jtr <taoranji@gmail.com>
2019-03-12 16:42:12 -0400

    api update

--- gen_index.py
+++ gen_index.py
@@ -6,7 +6,7 @@ import argparse
 import json
 import multiprocessing as mp
 
-def worker(opath, ipath):
+def worker(opath, ipath, index_es):
     opath = os.path.join(opath, os.path.basename(ipath) + '.index')
     with open(ipath, 'r') as ifp, open(opath, 'w') as ofp:
         for paper in ifp:
@@ -14,7 +14,7 @@ def worker(opath, ipath):
             if 'id' not in paper or 'year' not in paper:
                 continue
             pid, year = paper['id'], paper['year']
-            index = {'index': {'_index': 'publication', '_id': pid}}
+            index = {'index': {'_index': index_es, '_id': pid}}
             paper = {'year': year,
                      'title': paper['title'].lower().strip(),
                      'abstract': paper['abstract'].lower(),
@@ -28,14 +28,19 @@ def worker(opath, ipath):
 
 if __name__ == "__main__":
     pparser = argparse.ArgumentParser()
-    pparser.add_argument('opath', help='output path')
-    pparser.add_argument('ipaths', nargs='+', help='input paths')
+    pparser.add_argument('--index', type=str, default='mag_v1',
+                         help='index name used in elasticsearch')
+    pparser.add_argument('--cores', type=int, default=2,
+                         help='Number of cores to use')
+    pparser.add_argument('--output', required=True, help='output path')
+    pparser.add_argument('--input', required=True, nargs='+',
+                         help='input paths')
     args = pparser.parse_args()
-    if not os.path.exists(args.opath):
-        os.makedirs(args.opath)
-    pool = mp.Pool(4)
-    for ipath in args.ipaths:
-        res = pool.apply_async(worker, args=(args.opath, ipath,))
+    if not os.path.exists(args.output):
+        os.makedirs(args.output)
+    pool = mp.Pool(args.cores)
+    for ipath in args.input:
+        res = pool.apply_async(worker, args=(args.output, ipath, args.index, ))
     print(res.get())
     pool.close()
     pool.join()
--- load_data.sh
+++ load_data.sh
@@ -1,11 +1,12 @@
 # This script is used to create index and insert data.#
 
-index_name='publication'
+index_name=$1
+mag_es=$2
 # create index
 curl -X PUT "localhost:9200/$index_name" -H 'Content-Type: application/json' -d '{"settings": {"index" : {"number_of_shards" : 40}}, "mappings": {"doc": {"_field_names": {"enabled": false}, "properties": {"year": {"type": "integer"}, "title": {"type": "text"}, "abstract": {"type": "text"}}}}}'
 # update setting
 curl -X PUT "localhost:9200/$index_name/_settings" -H 'Content-Type: application/json' -d '{"index" : {"number_of_replicas" : 0, "refresh_interval" : -1}}'
 # insert index
-find /data/mag/mag.en.index.es/ -type f -print0 | xargs -0 -I {} -n 1 bash -c "curl -o /dev/null -s -H 'Content-Type: application/x-ndjson' -XPOST 'localhost:9200/$index_name/doc/_bulk?pretty' --data-binary @{}"
+find $mag_es -type f -print0 | xargs -0 -I {} -n 1 bash -c "curl -o /dev/null -s -H 'Content-Type: application/x-ndjson' -XPOST 'localhost:9200/$index_name/doc/_bulk?pretty' --data-binary @{}"
 # reset setting
 curl -X PUT "localhost:9200/$index_name/_settings" -H 'Content-Type: application/json' -d '{"index" : {"refresh_interval" : "1s"}}'
